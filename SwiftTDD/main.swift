d83df8db46d2ad93503c6d5c6484b9374e70a8b7
Adam Zielinski <github@maosmurf.com>
2020-05-27 20:24:00 +0200

    Hello world

---
+++ SwiftTDD/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftTDD
+//
+//  Created by Adam Zielinski on 27.05.20.
+//  Copyright © 2020 MAOSMURF. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
